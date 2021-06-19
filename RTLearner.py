2f52d5ef1dacbcd1004da65ef7d54702d9f5b8bf
Kabir Kang <kangkabir@gmail.com>
2021-06-18 18:56:50 -0700

    Removed default comments from LinRegLearner

--- RTLearner.py
+++ RTLearner.py
@@ -1,53 +1,14 @@
-""""""
-"""
-A simple wrapper for linear regression.  (c) 2015 Tucker Balch
-
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)
-Atlanta, Georgia 30332
-All Rights Reserved
-
-Template code for CS 4646/7646
-
-Georgia Tech asserts copyright ownership of this template and all derivative
-works, including solutions to the projects assigned in this course. Students
-and other users of this template code are advised not to share it with others
-or to make it available on publicly viewable websites including repositories
-such as github and gitlab.  This copyright statement should not be removed
-or edited.
-
-We do grant permission to share solutions privately with non-students such
-as potential employers. However, sharing with other current or future
-students of CS 7646 is prohibited and subject to being investigated as a
-GT honor code violation.
-
------do not edit anything above this line---
-"""
-
 import numpy as np
 from random import randrange
 
+
 class RTLearner(object):
-    """
-    This is a Linear Regression Learner. It is implemented correctly.
-
-    :param verbose: If “verbose” is True, your code can print out information for debugging.
-        If verbose = False your code should not generate ANY output. When we test your code, verbose will be False.
-    :type verbose: bool
-    """
-
     def __init__(self, leaf_size=1, verbose=False):
-        """
-        Constructor method
-        """
         self.leaf_size = leaf_size
         self.verbose = verbose
         self.tree = None
 
     def author(self):
-        """
-        :return: The GT username of the student
-        :rtype: str
-        """
         return "kkang68"
 
     def get_split_feature(self, data_x, data_y):
@@ -72,14 +33,6 @@ class RTLearner(object):
             return np.concatenate((root, left_tree, right_tree))
 
     def add_evidence(self, data_x, data_y):
-        """
-        Add training data to learner
-
-        :param data_x: A set of feature values used to train the learner
-        :type data_x: numpy.ndarray
-        :param data_y: The value we are attempting to predict given the X data
-        :type data_y: numpy.ndarray
-        """
         self.tree = self.build_tree(data_x, data_y)
 
     def query(self, points):
@@ -97,6 +50,7 @@ class RTLearner(object):
                     curr = curr + int(curr_node[3])
             result[i] = self.tree[curr][1]
         return result
+
 
 if __name__ == "__main__":
     print("the secret clue is 'zzyzx'")
